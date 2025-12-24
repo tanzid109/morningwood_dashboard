bb245e8decbab8b705418cf6bf4c6e116b6b4ec9
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-12-24 06:16:39 +0600

    next xonfig

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig = {
         hostname: "ddddddddddddddfffffff.s3.ap-southeast-1.amazonaws.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "my-streams-bucket-ap-south-1.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
     ],
   },
 };
